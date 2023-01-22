843129d74415e02635f0192f655778207fe3ec88
BuildTools <unconfigured@null.spigotmc.org>
2023-01-22 15:51:26 +0100

    Base Tests

---
+++ main.py
@@ -0,0 +1,3 @@
+import eel
+
+print("eel")
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="Labfox Launcher",
+      version="1.0",
+      description="Launcher des programmes LabFox",
+      author="LabFox",
+      author_email="labfoxdev@gmail.com",
+      url="https://labfox.fr/launcher")
